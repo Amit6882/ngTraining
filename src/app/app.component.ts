ffde4fb65df9efa09365efec147f090034ce4e69
Amit Dubey <akd6882@gmail.com>
2020-11-16 00:17:58 +0530

    default core features

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,21 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+
   title = 'basicNgApp';
+  
+  // data = {
+  //   title: 'basicNgApp'
+  // }
+
+  // data = {
+    // title: 'basicNgApp<h1>Attack</h1>'
+    // title: '<h1>basicNgApp</h1>'
+    // title: '<h1>basicNgApp</h1><script>alert("Attack")</script>'
+  // }
+
+  // onKeyup(newTitle: string) {
+  //   this.data.title = newTitle;
+  // }
+
 }
